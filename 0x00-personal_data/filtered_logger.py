0eb5eaf48dc356ae373e2b2672dc7f28ff28a083
hendrixxD <maticsjnr@gmail.com>
2023-03-02 23:07:09 +0100

    regexin

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,22 +3,14 @@
 personal data
 """
 
-#import re
-#from typing import List
-
-#def filter_datum(fields: List[str], redaction: str, message: str,
-#                 separator: str) -> str:
-#    """ Replacing """
-#    for f in fields:
-#        message = re.sub(rf"{f}=(.*?)\{separator}",
-#                         f'{f}={redaction}{separator}', message)
-#    return message
-
-
 import re
 from typing import List
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+
+def filter_datum(fields: List[str],
+                 redaction: str,
+                 message: str,
+                 separator: str) -> str:
     """
     Replaces specified fields in a log message with a redaction string.
 
