d6b83ae2520ffd6588380e0cdd5dac11a9da83eb
hendrixxD <maticsjnr@gmail.com>
2023-03-03 12:59:10 +0100

    logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -33,6 +33,26 @@ def filter_datum(fields: List[str],
         message = re.sub(pattern, redaction, message)
     return message
 
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """
+        Init
+        """
+        self.fields = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ """"
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
+
 
 def get_logger() -> logging.Logger:
     """ Implementing a logger.
