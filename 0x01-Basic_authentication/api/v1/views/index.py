ca75914813e8d64a9888bff3193135ce2331b08c
hendrixxD <maticsjnr@gmail.com>
2023-03-09 14:24:36 +0100

    pypi

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -7,8 +7,8 @@ from api.v1.views import app_views
 
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorised() -> str:
-  """return abort 401"""
-  abort(401)
+    """return abort 401"""
+    abort(401)
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
