1e0fe8260980d1d7d28502c7b27af04a0957d409
hendrixxD <maticsjnr@gmail.com>
2023-03-09 16:09:35 +0100

    auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -20,6 +20,7 @@ def not_found(error) -> str:
     """
     return jsonify({"error": "Not found"}), 404
 
+
 @app.errorhandler(401)
 def unauthorised(error) -> str:
     """Unauthorised
@@ -36,6 +37,7 @@ def forbidden(error) -> str:
 
 
 if __name__ == "__main__":
+    """ main """
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
     app.run(host=host, port=port)
