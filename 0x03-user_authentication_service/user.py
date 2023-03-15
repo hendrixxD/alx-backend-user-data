b9bafbd94cdde5bd2a5d3c3d03ba84c8b5579e4f
hendrixxD <maticsjnr@gmail.com>
2023-03-15 10:18:24 +0100

    no new line at the end of line

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
 """user model"""
 
-import flask
-from sqlalchemy import create_engine
+# from sqlalchemy import create_engine
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
-
-engine = create_engine('sqlite:///:memory:', echo=True)
+# engine = create_engine('sqlite:///:memory:', echo=True)
 
 
 class User(Base):
@@ -23,14 +21,4 @@ class User(Base):
     reset_token = Column(String(250), nullable=True)
 
 # User.__tablename__
-
-"""
-Table('users', MetaData(bind=None),
-            Column('id', Integer(), table=<users>, primary_key=True)
-            Column('email', String(), table=<users>, nullable=False),
-            Column('hashed_password', String(), table=<users>, nullable=False),
-            Column('session_id', String(), table=<users>, nullable=True),
-            Column('reset_token', String(), nullable=True))
-"""
-
 # Base.metadata.create_all(engine)
