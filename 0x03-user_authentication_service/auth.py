fcb5139fc042e955a9c79f613454dcd8598c41b3
hendrixxD <maticsjnr@gmail.com>
2023-03-17 10:41:39 +0100

    NEWID

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -50,3 +50,14 @@ class Auth:
             return False
         else:
             return bcrypt.checkpw(password.encode(), user.hashed_password)
+
+    def create_session(self, email: str) -> Union[None, str]:
+        """creates a session with a new uuid"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return None
+
+        session_id = _generate_uuid()
+        self._db.update_user(user.id, session_id=session_id)
+        return session_id
