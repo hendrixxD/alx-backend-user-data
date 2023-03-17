c8dea23cbc267501162cc98f8a540b86bf73b049
hendrixxD <maticsjnr@gmail.com>
2023-03-17 12:18:27 +0100

    valid login

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -5,7 +5,8 @@ from auth import Auth
 from flask import (
     Flask,
     jsonify,
-    request
+    request,
+    app
 )
 
 app = Flask(__name__)
@@ -31,6 +32,27 @@ def users() -> str:
         return jsonify({"email": f"{email}", "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login() -> str:
+    """implements login"""
+    try:
+        email = request.args.get('email')
+        password = request.args.get('password')
+    except KeyError:
+        abort(400)
+
+    try:
+        if AUTH.valid_login(email, password):
+            session_id = AUTH.create_session(email)
+            response = jsonify({"email": email, "message": "logged in"})
+            response.set_cookie('session_id', session_id)
+            return response
+        else:
+            abort(401)
+    except NoResultFound:
+        abort(401)
 
 
 if __name__ == "__main__":
